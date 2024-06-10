9c2a6e964d0e7272f0a78a9b4687e73bcc98947c
Gabriel Barros <ugabrieldev@gmai.com>
2024-06-10 17:00:10 -0300

    feat(user): :sparkles: add create users validation
    validation for types, structures and presence

--- src/http/controllers/users/create-user.ts
+++ src/http/controllers/users/create-user.ts
@@ -1,24 +1,44 @@
 import { Request, Response } from "express";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 import { makeCreateUser } from "../../../usecases/factories/make-create-user";
+import { UserAlreadyExistError } from "../../../usecases/errors/user-already-exist-error";
 
 export async function createUser(req: Request, res: Response) {
   const createUserBodySchema = z.object({
-    name: z.string(),
-    email: z.string(),
-    age: z.coerce.number(),
+    name: z.string().min(1, "Name is required"), // Adiciona uma mensagem de erro se o nome estiver vazio
+    email: z.string().email("Invalid email address"), // Verifica se o email é válido
+    age: z.coerce.number().positive("Age must be a positive number"), // Garante que a idade é um número positivo
   });
 
-  const { age, email, name } = createUserBodySchema.parse(req.body);
+  try {
+    const { age, email, name } = createUserBodySchema.parse(req.body);
 
-  const createUser = makeCreateUser();
+    const createUser = makeCreateUser();
 
-  await createUser.execute({
-    age,
-    email,
-    name,
-  });
+    await createUser.execute({
+      age,
+      email,
+      name,
+    });
 
-  return res.status(201).json({ message: "User created" });
+    return res.status(201).json({ message: "User created" });
+  } catch (error) {
+    // Tratando erros de validação
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        message: `Validation failed: ${error.errors.map((e) => {
+          return e.message;
+        })}`,
+      });
+    }
+
+    // Se email já existe
+    if (error instanceof UserAlreadyExistError) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    // Tratando outros erros
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
