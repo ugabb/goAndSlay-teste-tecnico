fa7195053cd30a6ffa736f015fc58170feb3cd07
Gabriel Barros <ugabrieldev@gmai.com>
2024-06-10 17:28:12 -0300

    feat: :sparkles: add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import { config } from "dotenv";
 import express from "express";
-
+import cors from "cors";
 import { userRoutes } from "./http/controllers/users/routes";
 
 // iniciar variaveis de ambiente
 config();
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(userRoutes);
