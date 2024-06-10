de2470382fb3a16bc5e5cffc9aebca52f4fef195
Gabriel Barros <ugabrieldev@gmai.com>
2024-06-10 17:04:57 -0300

    feat(user): :sparkles: add validations to update user

--- src/http/controllers/users/update-user.ts
+++ src/http/controllers/users/update-user.ts
@@ -1,25 +1,25 @@
 import { ResourceNotFoundError } from "./../../../usecases/errors/resource-not-found-error";
 import { Request, Response } from "express";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 import { makeUpdateUser } from "../../../usecases/factories/make-update-user";
 
 export async function updateUser(req: Request, res: Response) {
   const updateUserBodySchema = z.object({
-    name: z.string(),
-    email: z.string(),
-    age: z.coerce.number(),
+    name: z.string().min(1, "Name is required"), // Adiciona uma mensagem de erro se o nome estiver vazio
+    email: z.string().email("Invalid email address"), // Verifica se o email é válido
+    age: z.coerce.number().positive("Age must be a positive number"), // Garante que a idade é um número positivo
   });
+
   const updateUserParamsSchema = z.object({
-    id: z.string(),
+    id: z.string().uuid(), //valida se é um uuid
   });
-
-  const { age, email, name } = updateUserBodySchema.parse(req.body);
-  const { id } = updateUserParamsSchema.parse(req.params);
-
-  const updateUser = makeUpdateUser();
 
   try {
+    const { age, email, name } = updateUserBodySchema.parse(req.body);
+    const { id } = updateUserParamsSchema.parse(req.params);
+
+    const updateUser = makeUpdateUser();
     await updateUser.execute({
       id,
       age,
@@ -29,10 +29,20 @@ export async function updateUser(req: Request, res: Response) {
 
     return res.status(200).json({ message: "User updated" });
   } catch (error) {
+    // Tratando erros de validação
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        message: `Validation failed: ${error.errors.map((e) => {
+          return e.message;
+        })}`,
+      });
+    }
+
     if (error instanceof ResourceNotFoundError) {
       return res.status(404).json({ message: "User not found" });
-    } else {
-      return res.status(404).json({ message: error });
     }
+
+    // Tratando outros erros
+    return res.status(500).json({ message: "Internal server error" });
   }
 }
