c30efe44d2c9075f3d68ead6819fdd3ddace0737
Gabriel Barros <ugabrieldev@gmai.com>
2024-06-10 11:28:03 -0300

    fix(user): :children_crossing: add an feedback error if user not exist

--- src/http/controllers/users/delete-user.ts
+++ src/http/controllers/users/delete-user.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 import { makeDeleteUser } from "../../../usecases/factories/make-delete-user";
 import { z } from "zod";
+import { ResourceNotFoundError } from "../../../usecases/errors/resource-not-found-error";
 
 export async function DeleteUser(req: Request, res: Response) {
   const deleteUserParams = z.object({
@@ -10,9 +11,17 @@ export async function DeleteUser(req: Request, res: Response) {
 
   const { id } = deleteUserParams.parse(req.params);
 
-  const deleteUser = makeDeleteUser();
+  try {
+    const deleteUser = makeDeleteUser();
 
-  await deleteUser.execute({ id });
+    await deleteUser.execute({ id });
 
-  return res.status(200).json({ message: "User Deleted" });
+    return res.status(200).json({ message: "User Deleted" });
+  } catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      return res.status(404).json({ message: error });
+    }
+  }
 }
--- src/http/controllers/users/get-user.ts
+++ src/http/controllers/users/get-user.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 import { makeGetUser } from "../../../usecases/factories/make-get-user";
 import { z } from "zod";
+import { ResourceNotFoundError } from "../../../usecases/errors/resource-not-found-error";
 
 export async function GetUser(req: Request, res: Response) {
   const getUserParams = z.object({
@@ -10,9 +11,17 @@ export async function GetUser(req: Request, res: Response) {
 
   const { id } = getUserParams.parse(req.params);
 
-  const fetchUsers = makeGetUser();
+  try {
+    const fetchUsers = makeGetUser();
 
-  const user = await fetchUsers.execute({ id });
+    const user = await fetchUsers.execute({ id });
 
-  return res.status(200).json(user);
+    return res.status(200).json(user);
+  } catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      return res.status(404).json({ message: error });
+    }
+  }
 }
