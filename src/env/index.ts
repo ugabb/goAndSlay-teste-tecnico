4375375c94e9f49357d6003fdc6b399b1450caed
Gabriel Barros <ugabrieldev@gmai.com>
2024-06-10 17:10:05 -0300

    refactor: :art: clear env validation

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,16 +1,15 @@
-import { z } from "zod"
+import { z } from "zod";
 
 const envSchema = z.object({
-    PORT: z.coerce.number().default(3333),
-    DATABASE_URL: z.string().url()
-})
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url(),
+});
 
-const _env = envSchema.safeParse(process.env)
-console.log(process.env)
+const _env = envSchema.safeParse(process.env);
 
-if(_env.success === false){
-    console.log("❌ Invalid Enviroment Variables", _env.error);
-    throw new Error("❌ Invalid environment variables");
+if (_env.success === false) {
+  console.log("❌ Invalid Enviroment Variables", _env.error);
+  throw new Error("❌ Invalid environment variables");
 }
 
 export const env = _env.data;
